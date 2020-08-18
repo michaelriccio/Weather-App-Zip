a384dc642a88fdbc1060b69a188123a917f2de9a
michaelriccio <fchs219376@gmail.com>
2020-08-18 19:08:21 -0400

    Making process

--- public/js/app.js
+++ public/js/app.js
@@ -2,44 +2,81 @@
 let lat;
 let long;
 let weatherUrl;
+let hourlyUrl;
+let dailyUrl;
+let dailyForcast;
+let hourlyForcast;
+let pastLogs;
+let data1;
+let data2;
+let data3;
+const key = "5f6df12f283bc1a30cd52357ca119ed4";
 
-// Getting the user geolocation.
-if('geolocation' in navigator) {
-    console.log('geolocation is available');
-    navigator.geolocation.getCurrentPosition((position) => {
-        lat = position.coords.latitude;
-        long = position.coords.longitude;
-        console.log(lat);
-        console.log(long);
-        // Personal API Key for OpenWeatherMap API
-        weatherUrl = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=5f6df12f283bc1a30cd52357ca119ed4`;
-        console.log(weatherUrl);
-        
-    })
+let optionPost = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data1)
+};
 
-
-    
-} else {
-    console.log('geolocation is not avaliable');
-}
-
-
-// Event listener to add function to existing HTML DOM element
-/* Function called by event listener */
-
-getWeather().catch(error => {
-    console.log('error!');
-    console.error(error);
-});
+let optionGet = {
+        method: 'POST',
+        headers: {'Content-Type': 'applicaiton/json'},
+        body: JSON.stringify(dataHolder)
+};
 
 /* Function to GET Web API Data*/
 async function getWeather() {
-    const response = await fetch(weatherUrl);
-    console.log(response);
-    const data = await response.json();
+    const responseWeather = await fetch(weatherUrl);
+    data1 = await responseWeather.json();
+    const responseHourly = await fetch(hourlyUrl);
+    data2 = await responseHourly.json();
+    const responseDaily = await fetch(dailyUrl);
+    data3 = await responseDaily.json();
+    console.log(data1);
+    console.log(data2);
+    console.log(data3);
+};
+
+/* Function to GET Project Data */
+/* Function to POST Project Data */
+async function postJournal(jorn) {
+    let sent = fetch('/post', optionsPost);
+    
+};
+
+async function getJournal(){
+    let receive = fetch('/all', optionGet)
+
+}
+
+document.addEventListener('load', weatherGetter());
+
+/* Function called by event listener */
+function weatherGetter(){
+    // Getting the user geolocation.
+    if('geolocation' in navigator) {
+        console.log('geolocation is available');
+        navigator.geolocation.getCurrentPosition((position) => {
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+            // Personal API Key for OpenWeatherMap API
+            weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`;
+            hourlyUrl = `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${lat}&lon=${long}&appid=${key}`;
+            dailyUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&cnt=16&appid=${key}`;
+
+
+            // Calling function, catching errors.
+            getWeather().catch(error => {
+                console.log('error!');
+                console.error(error);
+            });
+        })
+    } else {
+        console.log('geolocation is not avaliable');
+    }
 };
 
 
-async function postLocation() {}
+// Event listener to add function to existing HTML DOM element
 
-/* Function to GET Project Data */
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
-const dataHolder = {};// Setup empty JS object to act as endpoint for all routes
+const dataHolder = [];// Setup empty JS object to act as endpoint for all routes
+const weatherJournal = {};
 
 const express = require('express');// Express to run server and routes
 
@@ -13,18 +14,14 @@ const cors = require('cors');/* Middleware*/
 app.use(cors());
 
 app.use(express.static('public'));// Initialize the main project folder
-app.use(express.json());
-// Spin up the server
-const port = 3000;
-app.listen(port, () => {console.log(`using localhost: ${port}`);});
+
 // Callback to debug
 
 // Initialize all route with a callback function
 // Callback function to complete GET '/all'
 app.get('/all', (request, response) => {
-    console.log(request.body);
-    let data = request.body;
-    dataHolder.push(data);
+    response = "GET response";
+    request.body = dataHolder;
 })
 
 // Post Route
@@ -38,3 +35,8 @@ app.post('/post', (request, response) => {
         longitude: data.long
     })
 });
+
+
+// Spin up the server
+const port = 3000;
+app.listen(port, () => {console.log(`using localhost: ${port}`);});
